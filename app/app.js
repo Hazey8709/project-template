1e8956d2bdcfa723249018b07b0447641bdedd2d
Hazey8709 <jfortin802@gmail.com>
2023-03-06 18:07:26 -0500

    ud

--- app/app.js
+++ app/app.js
@@ -23,15 +23,15 @@ app.use((req, res, next) => {
 //! Service Status
 //* localhost:4000/
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "service is up" });
-    console.log("service Is Up!");
+    res.status(200).json({ message: "connected" });
+    console.log("Connected!");
 });
 
 //!   Routers
 
 //! Error Handling
 app.use((req, res, next) => {
-    const error = new Error("not found");
+    const error = new Error("NOT FOUND");
     error.status = 404;
     next(error);
 });
@@ -41,6 +41,7 @@ app.use((error, req, res, next) => {
         error: {
             message: error.message,
             status: error.status,
+            method: req.method,
         },
     });
 });
