9c8c5b6a576cf0c2a6630f3dbb0b584aed019383
Hazey8709 <jfortin802@gmail.com>
2023-03-06 17:57:05 -0500

    ud

---
+++ app/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const app = express();
+
+//! middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//! CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Header",
+        "Origin, X-requested-with, Content-Type, Accept, Authorization"
+    );
+
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    }
+    next();
+});
+
+//! Service up
+//* localhost:4000/
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "service is up" });
+    console.log("service Is Up!");
+});
+
+//!   Routers
+
+//! Error Handling
+app.use((req, res, next) => {
+    const error = new Error("not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+            status: error.status,
+        },
+    });
+});
+
+module.exports = app;
